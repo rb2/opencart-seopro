8b202b5d77fd54a9775421ae54bd1207d691b49e
Sergey Ogarkov <sogarkov@gmail.com>
2015-01-14 15:17:18 +0200

    [~] Remove unused files

--- bin/install-inject-into-oc2.sh
+++ bin/install-inject-into-oc2.sh
@@ -26,29 +26,15 @@ BASE="$HOME/webproject/oc/oc2011/public_html"
 # ------------------------------------------------------------------------------------
 
 ## make structure of folders
-mkdir -p ${BASE}/catalog/controller/module/
-mkdir -p ${BASE}/catalog/view/theme/default/template/module/
-#mkdir -p ${BASE}/admin/language/english/module/
-#mkdir -p ${BASE}/admin/language/english/rbose/
-#mkdir -p ${BASE}/admin/language/russian/module/
-#mkdir -p ${BASE}/admin/language/russian/rbose/
-#mkdir -p ${BASE}/admin/model/module/
-#mkdir -p ${BASE}/admin/view/javascript/jquery/magnific/
-#mkdir -p ${BASE}/admin/view/stylesheet/rbose/
-#mkdir -p ${BASE}/admin/view/template/module/
-#mkdir -p ${BASE}/admin/view/template/rbose/
-
-#mkdir -p ${BASE}/catalog/language/russian/information
+#mkdir -p ${BASE}/admin/view/template/ocjazz/
 
 ## new module files
 cd ../upload
 ln -s --force `pwd`/catalog/controller/common/seo_pro.php                   ${BASE}/catalog/controller/common/seo_pro.php
-ln -s --force `pwd`/catalog/controller/module/language.php                  ${BASE}/catalog/controller/module/language.php
-ln -s --force `pwd`/catalog/view/theme/default/template/module/language.tpl ${BASE}/catalog/view/theme/default/template/module/language.tpl
 
 ## change existing core files
 cd ../dev-modified
-ln -s --force `pwd`/system/library/url.php                       ${BASE}/system/library/url.php
+ln -s --force `pwd`/index.php                                    ${BASE}/index.php
 ln -s --force `pwd`/admin/controller/catalog/product.php         ${BASE}/admin/controller/catalog/product.php
 ln -s --force `pwd`/admin/controller/setting/setting.php         ${BASE}/admin/controller/setting/setting.php
 ln -s --force `pwd`/admin/language/english/catalog/product.php   ${BASE}/admin/language/english/catalog/product.php
@@ -57,6 +43,5 @@ ln -s --force `pwd`/admin/model/catalog/category.php             ${BASE}/admin/m
 ln -s --force `pwd`/admin/model/catalog/product.php              ${BASE}/admin/model/catalog/product.php
 ln -s --force `pwd`/admin/view/template/catalog/product_form.tpl ${BASE}/admin/view/template/catalog/product_form.tpl
 ln -s --force `pwd`/admin/view/template/setting/setting.tpl      ${BASE}/admin/view/template/setting/setting.tpl
-ln -s --force `pwd`/index.php                                    ${BASE}/index.php
 ln -s --force `pwd`/catalog/controller/common/language.php       ${BASE}/catalog/controller/common/language.php
 ln -s --force `pwd`/catalog/view/theme/default/template/common/language.tpl   ${BASE}/catalog/view/theme/default/template/common/language.tpl
