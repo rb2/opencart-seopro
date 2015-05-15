12c14230420b2083a40acef35bf4103e93462954
Sergey Ogarkov <sogarkov@gmail.com>
2015-05-15 11:03:38 +0300

    [!] #5 The route information/information/agree was not filtered

--- upload/catalog/controller/common/seo_pro.php
+++ upload/catalog/controller/common/seo_pro.php
@@ -238,6 +238,7 @@ class ControllerCommonSeoPro extends Controller {
 
 			case 'product/product/review':
 			case 'information/information/info':
+			case 'information/information/agree':
 				return $link;
 				break;
 
