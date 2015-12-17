d926a2fc310d0db1f1a2609c11572a3162de0fde
Ruslan Brest <rb@labtodo.com>
2015-12-17 18:30:51 +0200

    [~] fix maintenance script (`rm $MODULE*.zip` removes backups too)

--- bin/deployment.sh
+++ bin/deployment.sh
@@ -4,7 +4,7 @@ MODULE=ocjazz-seopro-v203x
 cd ..
 git log --pretty=format:"%cd %s" --date=short --no-merges | grep -v '\[~\]'  > ./history.txt
 
-# rm -f $MODULE*.zip
+rm -f $MODULE.{ocmod,manual-install}.zip
 zip -r $MODULE.ocmod.zip install.xml upload README.* history.txt HOWTO-install-ocmod.*
 # zip -r $MODULE.manual-install.zip upload dev-modified README.* history.txt HOWTO-install-manual.*
 rm -f history.txt
