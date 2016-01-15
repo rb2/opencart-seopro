ddd339190c205bc64f0d86b9120036fe0c8fc1b2
Ruslan Brest <rb@labtodo.com>
2016-01-15 22:05:18 +0200

    [~] no more `manual-install.zip` will be deployed and supported: all manual setup can be done using extracted ocmod.zip

--- bin/deployment.sh
+++ bin/deployment.sh
@@ -4,7 +4,7 @@ MODULE=ocjazz-seopro-v2.x
 cd ..
 git log --pretty=format:"%cd %s" --date=short --no-merges | grep -v '\[~\]'  > ./history.txt
 
-# rm -f $MODULE*.zip
+rm -f $MODULE.ocmod.zip
 zip -r $MODULE.ocmod.zip install.xml upload README.* history.txt HOWTO-install-ocmod.*
-zip -r $MODULE.manual-install.zip upload dev-modified README.* history.txt HOWTO-install-manual.*
+# zip -r $MODULE.manual-install.zip upload dev-modified README.* history.txt HOWTO-install-manual.*
 rm -f history.txt
